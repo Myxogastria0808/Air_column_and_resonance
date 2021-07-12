7ea4b1c1b5e2dd0884669c224acfabf5780a0819
Myxogastria <myxogastria.0808@gmail.com>
2021-07-12 21:59:16 +0900

    Eighth push

--- en/urls.py
+++ en/urls.py
@@ -4,10 +4,10 @@ from .views import TitleView, PurposeView, PreparationView, MethodView, ResultVi
 urlpatterns = [
     path('', TitleView.as_view()),
     path('purpose/', PurposeView.as_view()),
-    path('preparation/', PreparationView.as_view()),
-    path('method/', MethodView.as_view()),
-    path('result/', ResultView.as_view()),
-    path('discussion/', DiscussionView.as_view()),
-    path('reference/', ReferenceView.as_view()),
+    path('purpose/preparation/', PreparationView.as_view()),
+    path('purpose/preparation/method/', MethodView.as_view()),
+    path('purpose/preparation/method/result/', ResultView.as_view()),
+    path('purpose/preparation/method/result/discussion/', DiscussionView.as_view()),
+    path('purpose/preparation/method/result/discussion/reference/', ReferenceView.as_view()),
 ]
 #leave these links
